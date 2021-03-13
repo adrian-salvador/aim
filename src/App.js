a9905b80c56c1cc432c05fb4f9502624f2093ba0
Adrian Salvador <adrian.salvador@uwaterloo.ca>
2021-03-13 15:56:53 -0500

    initiate todo component src file and update app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import NavigationBar from './components/NavigationBar'
+import Calendar from './components/Calendar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 
@@ -7,6 +9,10 @@ export default function App() {
   return (
     <>
       <NavigationBar />
+      <h1 align='center'> Let's Achieve Something Together Today</h1>
+      <Switch>
+        <Route path='/calendar' component={Calendar}/> 
+      </Switch>
 
     </>
   );
